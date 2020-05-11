db49034818e9f0e7839baf9ea5b0106cfae66ca9
Paul <paulbyrne99@rocketmail.com>
2020-05-11 23:10:25 +0100

    K nearest neighbours working

--- src/KNearest.py
+++ src/KNearest.py
@@ -9,37 +9,25 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import plot_confusion_matrix
 from pathlib import Path
-import os, sys
+from sklearn.metrics import accuracy_score
 
 # Load data
 
-print(os.getcwd())
 dataFolder = Path(".././data/")
 data = MNIST(dataFolder)
+data.gz = True
 images, labels = data.load_training()
 
 learning = KNeighborsClassifier()
 
 # Train the model
-trainX = images[:10000]
-trainY = labels[:10000]
+trainX = images[:20000]
+trainY = labels[:20000]
 learning.fit(trainX, trainY)
 
 # Test on the next 1000 images
-testX = images[10000:10100]
-expected = labels[10000:10100].tolist()
+testX = images[10000:11000]
+expected = labels[10000:11000].tolist()
 predicted = learning.predict(testX)
 
-titles_options = [("Confusion matrix", None),
-                  ("Normalized confusion matrix", 'true')]
-for title, normalize in titles_options:
-    disp = plot_confusion_matrix(predicted, testX, expected,
-                                 #display_labels=class_names,
-                                 cmap=plt.cm.Blues,
-                                 normalize=normalize)
-    disp.ax_.set_title(title)
-
-    print(title)
-    print(disp.confusion_matrix)
-
-plt.show()
+print("K Nearest Neighbours accuracy: ", accuracy_score(expected, predicted))
