b7428d285f638d9e9a4f6090df2cc29a4839b92c
Paul <paulbyrne99@rocketmail.com>
2020-05-12 17:09:23 +0100

    heatmap works, needs formatting work

--- src/KNearest.py
+++ src/KNearest.py
@@ -10,9 +10,10 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import plot_confusion_matrix
 from pathlib import Path
 from sklearn.metrics import accuracy_score
+import seaborn as sn
+import pandas as pd
 
 # Load data
-
 dataFolder = Path(".././data/")
 data = MNIST(dataFolder)
 data.gz = True
@@ -31,3 +32,16 @@ expected = labels[10000:11000].tolist()
 predicted = learning.predict(testX)
 
 print("K Nearest Neighbours accuracy: ", accuracy_score(expected, predicted))
+
+confusion=metrics.confusion_matrix(expected,predicted)
+print(confusion)
+plt.matshow(confusion, cmap='binary')
+
+# Seaborn heatmap as a confusion matrix
+df = pd.DataFrame(confusion, range(10), range(10))
+plt.figure(figsize=(10,7))
+sn.set(font_scale=1.4) # for label size
+confFig = sn.heatmap(df, annot=True, annot_kws={"size": 16}) # font size
+# Save figure to file
+fig = confFig.get_figure()
+fig.savefig("KNearest.png")
